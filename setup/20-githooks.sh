e7777751c021414932eab220edb5c9cd3e9a90b4
bmaximilian <maximilianbmail@gmail.com>
2018-09-01 16:41:56 +0200

    Setup

---
+++ setup/20-githooks.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+cd ..
+
+cd .git/hooks
+rm -f pre-push
+ln -s ../../setup/git/hooks/pre-push pre-push
---
+++ setup/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+./10-nodexec.sh
+./20-githooks.sh
